df1450da0892121c416a129ff0f1890c4a3c9ee3
hongvuong2017@gmail.com <hongvuong2017@gmail.com>
2019-10-31 12:42:09 +0700

    implemented

---
+++ FloralShop/Entity/Bill_detail.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FloralShop.Entity
+{
+    public class Bill_detail : Base
+    {
+        [Key]
+        public int ID { get; set; }
+        public int ID_Bill { get; set; }
+        public int ID_Product { get; set; }
+        public int quantity { get; set; }
+        public int Unit_price { get; set; }
+        public DateTime CreateTime { get; set; }
+        public DateTime UpdateTime { get; set; }
+        public bool IsDelete { get; set; }
+    }
+}
