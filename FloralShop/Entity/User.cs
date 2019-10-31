5591888557b8435bba19fe0cd4ef2c99c8df19cd
lxthao288@gmail.com <lxthao288@gmail.com>
2019-10-31 13:08:13 +0700

    thAO

---
+++ FloralShop/Entity/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FloralShop.Entity
+{
+    public class User : Base
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+        public string name { get; set; }
+        public bool gender { get; set; }
+        public string email { get; set; }
+        public string address { get; set; }
+        public int phone { get; set; }
+    }
+}
